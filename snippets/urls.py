3bf73f6bbe8e6b64dfcf5e4bb189c6dbc2fe2759
tbsmcd <tbsmcd@gmail.com>
2022-03-25 16:23:45 +0900

    6.1_Using_Routers

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,37 +1,15 @@
-from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from snippets.views import SnippetViewSet, UserViewSet, api_root
-from rest_framework import renderers
+from snippets.views import SnippetViewSet, UserViewSet
 
-snippet_list = SnippetViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-snippet_detail = SnippetViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'patch': 'partial_update',
-    'delete': 'destroy'
-})
-snippet_highlight = SnippetViewSet.as_view({
-    'get': 'highlight'
-}, renderer_classes=[renderers.StaticHTMLRenderer])
-user_list = UserViewSet.as_view({
-    'get': 'list'
-})
-user_detail = UserViewSet.as_view({
-    'get': 'retrieve'
-})
 
-urlpatterns = format_suffix_patterns([
-    path('', api_root),
-    path('snippets/', snippet_list, name='snippet-list'),
-    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
-    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
-    path('users/', user_list, name='user-list'),
-    path('users/<int:pk>', user_detail, name='user-detail'),
+# ViewSets を使っているので下記のように簡略化できる
+router = DefaultRouter()
+router.register(r'snippets', SnippetViewSet, basename='snippets')
+router.register(r'users', UserViewSet, basename='users')
+
+urlpatterns = [
+    # path('', api_root), <- 削除可能。DefaultRouter がやってくれる
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
-    # https://github.com/encode/django-rest-framework/blob/master/rest_framework/urls.py
-    # API で追加するだけならこの URL を使う必要もなさそう
-    # MEMO: admin/fukuoka123
-])
+]
